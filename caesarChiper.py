5a6ccdf7566ea14058f3e0370700629e6a0cd05b
corniergown <gerardohgomez0112@gmail.com>
2024-02-05 17:41:19 -0500

    add algorithm Caesar Chiper

--- caesarChiper.py
+++ caesarChiper.py
@@ -1,2 +1,13 @@
-def saludar():
-    print("Soy el archivo CaesarChiper")
+def caesar_chiper(text, space) -> str:
+    code_text = []
+
+    for char in text:
+        if ( char.isalpha() ):
+            if ( char.isupper() ):
+                code_text.append(chr(((ord(char) - 65 + space) % 26) + 65))  # Manejar letras mayúsculas
+            else:
+                code_text.append(chr(((ord(char) - 97 + space) % 26) + 97))  # Manejar letras minúsculas
+        else:
+            code_text.append(char)
+    
+    return ''.join(code_text)
--- main.py
+++ main.py
@@ -6,26 +6,38 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def menu():
-    print("Cifrado de texto!")
+    print("\nCifrado de texto!\n")
     print("1. Caesar Chiper")
     print("2. Vigenere Chiper")
     print("3. Salir")
 
 def main():
-    opt = int(input("Ingresa una opcion: "))
+    menu()
+
+    opt = int(input("\n~ Ingresa una opcion: "))
+    clear_screen()
 
     if (opt == 1):
-        codeCaesar.saludar()
-        
+        text = str(input("\n~ Ingresa el texto a cifrar: "))
+        shifts = int(input("~ Ingresa el numero de desplazamientos: "))
+
+        clear_screen()
+        print(f"\n[*] Texto cifrado: {codeCaesar.caesar_chiper(text, shifts)}")
+
     elif (opt == 2):
         # Entradad de datos para el cifrado de Vigenere
-        text = str(input("~ Ingresa el texto a cifrar: "))
+        text = str(input("\n~ Ingresa el texto a cifrar: "))
         key = str(input("~ Ingresa la clave: "))
 
-        print(f"[*] Texto cifrado: {codeVigenere.vigenere_chiper(text, key)}")
+        clear_screen()
+        print(f"\n[*] Texto cifrado: {codeVigenere.vigenere_chiper(text, key)}")
 
     elif (opt == 3):
         print("Saliendo . . .\n")
+
+    else:
+        print("\nOpcion ivalida!")
+        return 0
 
 if __name__ == '__main__':
     main()
